211a89d7821caac30441a259a626eb52b26f01b8
Shruti Yadav <shrutirathore8006@gmail.com>
2025-03-03 11:23:40 +0530

    unwanted import removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import "./App.css";
 import ContentSection from "./templates/components/Content-section";
 import HeroSections from "./templates/components/hero-section";
-import { useState } from "react";
-import { useInView } from "react-intersection-observer";
-import PassengerSection from "./templates/Slider/PassengerSection";
-import CommercialSection from "./templates/Slider/CommercialSection";
 import ContactForm from "./templates/components/ContactForm";
 import Footer from "./templates/components/Footer";
 
@@ -17,8 +13,6 @@ function App() {
    <ContentSection/>
    <ContactForm/>
    <Footer/>
-
-
     </>
   );
 }
